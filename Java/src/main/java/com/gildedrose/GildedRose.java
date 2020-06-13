8bfca4ea361ac74c9c5b13363e2440edc4bbf242
faustin0 <dinatalefausto@gmail.com>
2020-06-13 14:08:01 +0200

    WIP, working on an optional chain

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,10 @@
 package com.gildedrose;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 class GildedRose {
     Item[] items;
 
@@ -7,56 +12,115 @@ class GildedRose {
         this.items = items;
     }
 
+    public Optional<Item> getItemByName(String name) {
+        return Stream.of(items)
+                .filter(item -> item.name.equals(name))
+                .findFirst();
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
+
+
+        ItemUpdater itemUpdater = Stream.<Function<Item, Optional<ItemUpdater>>>of(
+                this::maxQualityGuard,
+                this::legendaryGuard,
+                this::decreaser
+        ).map(f -> f.apply(items[2]))
+                .reduce(Optional.empty(), this::ap)
+                .orElse(ItemUpdater.noOp());
+
+
+        itemUpdater.run();
+    }
+
+
+    Optional<ItemUpdater> maxQualityGuard(Item toCheck) {
+        return toCheck.quality > 50
+                ? Optional.empty()
+                : Optional.of(ItemUpdater.noOp());
+    }
+
+
+    Optional<ItemUpdater> legendaryGuard(Item toCheck) {
+        if (toCheck.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return Optional.empty();
+        }
+        return Optional.of(ItemUpdater.noOp());
+    }
+
+    Optional<ItemUpdater> decreaser(Item item) {
+        return Optional.of(() -> item.quality--);
+    }
+
+
+    public Optional<ItemUpdater> ap(Optional<ItemUpdater> o1, Optional<ItemUpdater> o2) {
+        return o1.flatMap(itemUpdater1 -> o2.map(itemUpdater2 -> itemUpdater1.andThen(itemUpdater2)));
+    }
+
+    @FunctionalInterface
+    public interface ItemUpdater extends Runnable {
+
+        static ItemUpdater noOp() {
+            return () -> {
+            };
+        }
+
+        default ItemUpdater andThen(Runnable after) {
+            Objects.requireNonNull(after);
+            return () -> {
+                run();
+                after.run();
+            };
+        }
+
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,64 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.getItemByName("foo")).isPresent();
     }
 
+    @Test
+    void AgedBrie_shouldIncreaseQuality() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+        assertEquals("Aged Brie", app.items[0].name);
+    }
+
+    @Test
+    void qualityShouldNeverExceed50() {
+        Item[] items = new Item[]{new Item("expensive", 0, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+
+        assertThat(app.getItemByName("expensive").get().quality)
+                .isLessThan(50);
+    }
+
+    @Test
+    void guardCheck() {
+        Item[] items = new Item[]{
+                new Item("Sulfuras, Hand of Ragnaros", 0, 40),
+                new Item("expensive", 0, 50),
+                new Item("normal", 0, 5),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertThat(app.getItemByName("expensive").get().quality)
+                .isLessThan(50);
+    }
+
+    @Test
+    void applicativeTest() {
+        Optional<GildedRose.ItemUpdater> itemUpdater1 = Optional.of(GildedRose.ItemUpdater.noOp());
+        Optional<GildedRose.ItemUpdater> itemUpdater2 = Optional.empty();
+
+        Optional<GildedRose.ItemUpdater> ap = new GildedRose(new Item[]{}).ap(itemUpdater1, itemUpdater2);
+        assertThat(ap).isEmpty();
+    }
 }
